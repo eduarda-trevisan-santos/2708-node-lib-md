75c3a2f376e7a029929517109bba75efa0653cf2
eduarda-trevisan-santos <eduarda.trevisan.santos@escola.pr.gov.br>
2026-05-21 13:49:21 +0000

    Trabalho 2 Finalizado

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
+import fs from 'fs';
 import chalk from 'chalk';
 
-console.log (chalk.cyan('Olha, eu não queria ser um meio-sangue'))
-console.log('Olha, eu não queria ser um meio-sangue');
+function trataErro(erro) {
+    console.log(erro);
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
 
+ // async/await
+
+ async function pegaArquivo(caminhoDoArquivo) {
+ try {
+  const encoding = 'utf-8';
+  const texto = await fs.promises.readFile
+  (caminhoDoArquivo, encoding)
+  console.log(chalk.green(texto));
+  } catch (erro) {
+    trataErro(erro)
+  }
+}
+
+
+// promises com then()
+
+// function pegaArquivo (caminhoDoArquivo) {
+  // const encoding = 'utf-8';
+   //fs.promises
+   //.readFile(caminhoDoArquivo, encoding)
+   //.then((texto)) => console.log(chalk.green(texto)))
+   //.catch (trataErro)
+ // }
+
+//function pegaArquivo(caminhoDoArquivo) {
+//    const encoding = 'utf-8';
+//    fs.readFile(caminhoDoArquivo, encoding, (erro, texto)  => {
+//        if (erro) {
+//          trataErro(erro);
+// }
+//    console.log(chalk.green(texto));
+ // })
+// }
+
+pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/');
