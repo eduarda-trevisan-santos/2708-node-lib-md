0a50f1a20ca31673c6f655e167f0a8ab5f5dda4f
eduarda-trevisan-santos <eduarda.trevisan.santos@escola.pr.gov.br>
2026-06-09 15:16:30 +0000

    Trabalho 3 finalizado

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+const textoTeste = 'São geralmente recuperados a partir de um objeto [Percy Jackson](https://br.pinterest.com/pablooncer/percy-jackson-s%C3%A9rie/) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<Percabeth>](https://br.pinterest.com/search/pins/?q=percabeth&rs=ac&len=3&source_id=ac_2xEMmqpS&eq=per&etslf=9704), a partir do objeto [Izuku Midoriya](https://br.pinterest.com/ideas/midoriya-izuku/933971132069/) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [Era uma vez um coração partido](https://es.pinterest.com/naiaramuniz56/era-uma-vez-um-cora%C3%A7%C3%A3o-partido/). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Principe Cruel](https://pt.pinterest.com/matildelanca123bom/pr%C3%ADncipe-cruel/) para mais informações.).'
+
+function extraiLinks(texto) {
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  const capturas = texto.match(regex);
+  console.log(capturas);
+
+}
+
+extraiLinks(textoTeste);
+
 function trataErro(erro) {
     console.log(erro);
     throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
@@ -40,5 +51,6 @@ function trataErro(erro) {
  // })
 // }
 
-pegaArquivo('./arquivos/texto.md');
-pegaArquivo('./arquivos/');
+// pegaArquivo('./arquivos/texto.md');
+
+// \[^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)
